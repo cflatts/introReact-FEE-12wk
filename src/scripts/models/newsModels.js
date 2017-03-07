5b1ccb13654bc18ac1406945552ae3181a63ca50
Caitlin Flattery <caitlinflattery@gmail.com>
2017-03-07 11:19:40 -0600

    created and exported news collection

--- src/scripts/models/newsModels.js
+++ src/scripts/models/newsModels.js
@@ -1,2 +1,13 @@
 import Backbone from 'backbone'
 import $ from 'jquery'
+
+const ArticleCollection=Backbone.Collection.extend({
+    url: 'https://api.nytimes.com/svc/search/v2/articlesearch.json',
+    _key: '11eaa2ee2ebb78f1cfb25971ad39c74d',
+
+    parse: function(apiResp) {
+        return apiResp.response.docs
+    }
+})
+
+export ArticleCollection
