681a90de0335ecd0cc641543484da47013344b37
Caitlin Flattery <caitlinflattery@gmail.com>
2017-03-07 10:57:51 -0600

    created skeleton of backbone router

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -8,8 +8,33 @@ const app = function() {
   document.querySelector('.container').innerHTML = `<h1>introReact</h1>`
 }
 
+var Router = Backbone.Controller.extend({
+    routes: {
+        'home': '_goHome',
+        'detail/:articleID': '_showDetailPage',
+        'search/:query': '_doArticleSearch',
+        '*catchall': '_redirect'
+    },
+
+    _goHome: function() {
+
+    },
+
+    _showDetailPage: function(articleID) {
+
+    },
+
+    _doArticleSearch: function(query) {
+
+    },
+
+    initialize: function() {
+        Backbone.history.start()
+    }
+})
+
 // x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..
-// NECESSARY FOR USER FUNCTIONALITY. DO NOT CHANGE. 
+// NECESSARY FOR USER FUNCTIONALITY. DO NOT CHANGE.
 export const app_name = init()
 app()
 // x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..
