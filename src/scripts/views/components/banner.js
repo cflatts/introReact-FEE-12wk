533ab489f32d49f9e160a7b1ea5511d766ea77ca
Caitlin Flattery <caitlinflattery@gmail.com>
2017-03-07 11:36:03 -0600

    created components directory, with banner component, wrote banner component

---
+++ src/scripts/views/components/banner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Banner=React.createClass({
+    render: function() {
+        return (
+            <div className='banner'>
+                <h1>The People`'`s News</h1>
+                <input type='text' onKeyDown={this._handleSearch}
+            </div>
+        )
+    }
+})
