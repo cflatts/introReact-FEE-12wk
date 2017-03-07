04c8231fb7ec0fdb3e09df9a0b86ec9650f08403
Caitlin Flattery <caitlinflattery@gmail.com>
2017-03-07 11:45:36 -0600

    wrote handle search method for banner

--- src/scripts/views/components/banner.js
+++ src/scripts/views/components/banner.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 
 const Banner=React.createClass({
+
+    _handleSearch: function(evt) {
+        evt.preventDefault()
+
+        evt.keyCode === 13 ? location.hash = `search/${evt.target.value}`: location.hash='home'
+    },
+
     render: function() {
         return (
             <div className='banner'>
@@ -12,3 +19,5 @@ const Banner=React.createClass({
         )
     }
 })
+
+export default Banner
