a9cfbd0c88ed8a8ffbb418d47b93256569f7eceb
Caitlin Flattery <caitlinflattery@gmail.com>
2017-03-08 12:31:03 -0600

    set up toggle to display paragraph by using state

--- src/scripts/views/articleView.js
+++ src/scripts/views/articleView.js
@@ -35,12 +35,35 @@ const SearchResults=React.createClass({
 })
 
 const Article=React.createClass({
+
+    _toggleParagraph: function() {
+        this.setState({
+            pShowing: true
+        })
+    },
+
+    getInitialState: function() {
+        return {
+            pShowing: false
+        }
+    },
+
     render: function() {
+
+        let paraStyle={
+            display: 'none'
+        }
+
+        if(this.state.pShowing) {
+            paraStyle.display = 'block'
+        }
+
         return (
             <div className='article'>
                 <h3>{this.props.model.get('headline').main}</h3>
                 <h5>{this.props.model.get('byline').original}</h5>
                 <button>+</button>
+                <p style={paraStyle} onClick={this._toggleParagraph}>{this.props.model.get('lead_paragraph')}</p>
                 <hr/>
             </div>
         )
