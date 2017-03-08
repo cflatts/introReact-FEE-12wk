f7c408a1c6bbb8bbea80eb5408d6f40f8fc496dc
Caitlin Flattery <caitlinflattery@gmail.com>
2017-03-08 11:10:39 -0600

    created new search results component and passed props down to it

--- src/scripts/views/articleView.js
+++ src/scripts/views/articleView.js
@@ -4,11 +4,20 @@ import Banner from './components/banner'
 
 const ArticleView=React.createClass({
     render: function() {
-        console.log(this)
-        
         return (
             <div className='articleView'>
                 <Banner/>
+                <SearchResults articleColl={this.props.collection}/>
+            </div>
+        )
+    }
+})
+
+const SearchResults=React.createClass({
+    render: function() {
+        return (
+            <div className='searchResults'>
+                
             </div>
         )
     }
